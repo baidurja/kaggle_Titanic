7d8958c34b9858294242b106b7e6fd0009478127
baidurja <39422735+baidurja@users.noreply.github.com>
2018-06-17 18:37:24 -0500

    Add files via upload

--- temp.py
+++ temp.py
@@ -9,23 +9,24 @@ import numpy as py
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def preprocess( data, offset ):
-    X = data.iloc[ :, [ 2 + offset, 4 + offset, 5 + offset ] ].values
+def preprocess( dataset, offset ):
+    X = dataset.iloc[ :, [ 2 + offset, 4 + offset, 5 + offset, 6 + offset, 7 + offset, 9 + offset, 11 + offset ] ].values
     from sklearn.preprocessing import LabelEncoder, OneHotEncoder
     labelencoder_X = LabelEncoder()
     X[ :, 1 ] = labelencoder_X.fit_transform( X[ :, 1 ] )
+    X[ :, 6 ] = labelencoder_X.fit_transform( X[ :, 6 ] )
     from sklearn.preprocessing import Imputer
     imputer = Imputer( missing_values = 'NaN', strategy = 'mean', axis = 0 )
     imputer = imputer.fit( X )
     X = imputer.transform( X )
     from sklearn.preprocessing import StandardScaler
     sc_X = StandardScaler()
-    X_toscale = X[ :, 2 ]
-    X_toscale = X_toscale[ :, None ]
+    X_toscale = X[ :, [2, 3, 4, 5] ]
+    #    X_toscale = X_toscale[ :, None ]
     X_toscale = sc_X.fit_transform( X_toscale )
-    X_toscale.shape = X[ :, 2 ].shape
-    X[ :, 2 ] = X_toscale
-    onehotencoder = OneHotEncoder( categorical_features = [0] )
+    #X_toscale.shape = X[ :, [2, 3, 4, 5] ].shape
+    X[ :, [2, 3, 4, 5] ] = X_toscale
+    onehotencoder = OneHotEncoder( categorical_features = [0, 6] )
     X = onehotencoder.fit_transform(X).toarray()
     
     return X
@@ -90,9 +91,19 @@ def visualize( data ):
     plt.show()
 
 dataset = pd.read_csv('train.csv')
+dataset.Embarked = dataset.Embarked.fillna( 'S' )
+
+#offset = 0
+#X = dataset.iloc[ :, [ 2 + offset, 4 + offset, 5 + offset, 6 + offset, 7 + offset, 9 + offset, 11 + offset ] ].values
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#labelencoder_X = LabelEncoder()
+#X[ :, 1 ] = labelencoder_X.fit_transform( X[ :, 1 ] )
+#X[ :, 6 ] = labelencoder_X.fit_transform( X[ :, 6 ] )
+
+
+X = preprocess( dataset, 0 )
 dev_dataset = pd.read_csv( 'test.csv' )
 dev_X = preprocess( dev_dataset, -1 )
-X = preprocess( dataset, 0 )
 #X = dataset.iloc[ :, [ 2, 4, 5 ] ].values
 y = dataset.iloc[ :, 1 ].values
 
@@ -129,7 +140,7 @@ visualize( dataset )
 #X = onehotencoder.fit_transform(X).toarray()
 
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.25, random_state = 0 )
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size = 0.2, random_state = 100 )
 
 algo = "SVM";
 
@@ -140,7 +151,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 if (algo == "SVM"):
-    classifier = SVC( C=10, kernel = 'rbf', random_state=0)
+    classifier = SVC( C=1, kernel = 'rbf', gamma = 0.143, random_state=0)
 elif ( algo == "NaiveBayes"):
     classifier = GaussianNB()    
 elif ( algo == "DecisionTree"):
@@ -158,14 +169,16 @@ print("Mean accuracy = ", scores.mean())
 print("SD accuracy = ", scores.std())
 
 from sklearn.model_selection import GridSearchCV
-parameters = [ {'C': [ 1.0, 2.0, 4.0, 4.3, 4.5, 10, 11, 12 ], 'kernel': ['rbf'], 'gamma': [ 1/2.5, 1/3, 1/3.5, 1/4, 1/4.5, 1/5, 1/5.5, 1/6 ] }]
-grid_search = GridSearchCV( estimator = classifier,
-                           param_grid = parameters,
-                           scoring = 'accuracy',
-                           cv = 10 )
-grid_search = grid_search.fit( X_train, y_train)
-best_accuracy = grid_search.best_score_
-best_params = grid_search.best_params_
+if ( algo == "SVM" ):
+    parameters = [ {'C': [ 1.0, 2.0, 4.0, 0.5, 0.75, 10, 11, 12 ], 'kernel': ['rbf'], 'gamma': [ 1/5, 1/6, 1/6.5, 1/7, 1/8 ] }]
+    grid_search = GridSearchCV( estimator = classifier,
+                               param_grid = parameters,
+                               scoring = 'accuracy',
+                               cv = 10 )
+    grid_search = grid_search.fit( X_train, y_train)
+    best_accuracy = grid_search.best_score_
+    best_params = grid_search.best_params_
+    print( best_params )
 
 classifier.fit(X_train, y_train)
 y_train_pred = classifier.predict( X_train )
@@ -195,7 +208,7 @@ print( classification_report( y_test, y_test_pred))
 y_dev_pred = classifier.predict( dev_X )
 
 sub = py.column_stack( ( dev_dataset.iloc[ :, 0 ].values, y_dev_pred ) )
-py.savetxt( "gender_submission.csv", sub.astype( int ), fmt = "%i", delimiter=",", header = "PassengerId, Survived", comments = '' )
+py.savetxt( "gender_submission.csv", sub.astype( int ), fmt = "%i", delimiter=",", header = "PassengerId,Survived", comments = '' )
 
 #plt.scatter( X_train, y_train, color = 'red' )
 #plt.plot( X_train, y_train_pred, color = 'blue' )
